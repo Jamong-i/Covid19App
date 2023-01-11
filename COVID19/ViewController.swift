e5a8dadacc48ba5c50b949044c5b1bb779d4ca56
Jamong-i <smkim961211@gmail.com>
2023-01-11 18:38:20 +0900

    Feat(iOS): API connection using Alamofire
    Alamofire를 이용하여 Good bye Covid API를 연결

--- COVID19/ViewController.swift
+++ COVID19/ViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+
+import Alamofire
 import Charts
 
 class ViewController: UIViewController {
@@ -17,7 +19,41 @@ class ViewController: UIViewController {
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view.
+		self.fetchCovidOverview(completionHandler: { [weak self] result in
+			guard let self = self else { return }
+			switch result {
+			case let .success(result):
+				debugPrint("success \(result)")
+			case let .failure(error):
+				debugPrint("error \(error)")
+			}
+		})
+	}
+	
+	func fetchCovidOverview(
+		completionHandler: @escaping (Result<CityCovidOverview, Error>) -> Void
+	) {
+		let url = "https://api.corona-19.kr/korea/country/new/"
+		let param = [
+			"serviceKey": "XSLRzO52NmhF4xtZHvouDbUTClPgJrnaf"
+		]
+		
+		AF.request(url, method: .get, parameters: param)
+			.responseData(completionHandler: { response in
+				switch response.result {
+				case let .success(data):
+					do {
+						let decoder = JSONDecoder()
+						let result = try decoder.decode(CityCovidOverview.self, from: data)
+						completionHandler(.success(result))
+					} catch {
+						completionHandler(.failure(error))
+					}
+					
+				case let .failure(error):
+					completionHandler(.failure(error))
+				}
+			})
 	}
 
 
